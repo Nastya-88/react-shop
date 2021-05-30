fd048f165689508f02fb4573ba5d2dcc30a6ebe8
Анастасия <shishlakovanastya@gmail.com>
2021-05-30 23:21:22 +0300

    gh-pages add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
         <div className="nav-wrapper">
             <a href="#" className="brand-logo">React Shop</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="!#">Repo</a></li>
+                <li><a href="https://nastya-88.github.io/react-shop/">Repo</a></li>
 
             </ul>
         </div>
